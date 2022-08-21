4d93670dfe1bd2c00144579c83c9073f8b2ff228
KIxn <kianreddy67@gmail.com>
2022-08-21 20:38:53 +0200

    Set up Routing + Store

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,6 +3,6 @@ import App from '../pages/App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/homepage/i);
   expect(linkElement).toBeInTheDocument();
 });
