849c5ecb714d14ab058240e65f72606a931807ff
dkb18 <dkbhoora01@gmail.com>
2022-08-27 02:12:01 +0200

    Sign up page UI

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,45 @@
+import '../styles/Signup.css'
+
+
 function Signup(){
     return(
-        <h1>Signin</h1>
+        <div className="SignupBox">
+            <h1>Create an account with Wits-Overflow</h1>
+            <div className="witsLogo"></div>
+
+            <form className="SignUpForm">
+                <div className="grid-container">
+                    <div className="group">
+                        <input className="input" type="text" required/>  
+                        <span className="bar"></span>
+                        <label className='label'>First name</label>
+                    </div>
+
+                    <div class="group">      
+                        <input className="input" type="text" required/>
+                        
+                        <span className="bar"></span>
+                        <label className='label'>Last name</label>
+                    </div>
+
+                    <div class="group">      
+                        <input className="input" type="text" required/>
+                        
+                        <span className="bar"></span>
+                        <label className='label'>Email</label>
+                    </div>
+
+                    <div class="group">      
+                        <input className="input" type="password" required/>
+                        
+                        <span className="bar"></span>
+                        <label className='label'>Password</label>
+                    </div>
+                </div>
+            </form>
+            <button className='loginBtn'>Login</button>
+
+        </div>
     )
 }
 
