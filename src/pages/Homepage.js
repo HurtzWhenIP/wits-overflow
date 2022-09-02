7b360d5b4567dcc396ed839d26fa501f4df122cd
Kian <kianreddy67@gmail.com>
2022-09-02 14:51:04 +0200

    true

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,30 @@
-
-import { useState } from "react";
-import "../styles/Homepage.css";
-
+import useAxios from "../hooks/useAxios";
+import axios from '../apis/ForumServer';
+import { useState,useEffect } from "react";
 
 function Homepage() {
+    //state to hold question data
+    const [data,setData] = useState([]);
     const [isOpen, setIsopen] = useState(false);
     const [query, setQuery] = useState("All");
+
+    const [response,error,loading,refetch] = useAxios({
+        axiosInstance: axios,
+        method: 'GET',
+        url: 'getAllQuestions.php',
+    });
+
+    useEffect(() => {
+        const result = Array.isArray(response);
+        if(response && (result.length>0)){
+            console.log("Questions stored");
+            setData(result);
+        }
+
+        return(() => {
+            console.log(data);
+        })
+    },[response,data])
 
     return (
         <div className="wrapper">
@@ -39,36 +58,5 @@ function Homepage() {
 
 
     )};
-import useAxios from "../hooks/useAxios";
-import axios from '../apis/ForumServer';
-import { useState,useEffect } from "react";
-
-function Homepage(){
-
-    //state to hold question data
-    const [data,setData] = useState([]);
-
-    const [response,error,loading,refetch] = useAxios({
-        axiosInstance: axios,
-        method: 'GET',
-        url: 'getAllQuestions.php',
-    });
-
-    useEffect(() => {
-        const result = Array.isArray(response);
-        if(response && (result.length>0)){
-            console.log("Questions stored");
-            setData(result);
-        }
-
-        return(() => {
-            console.log(data);
-        })
-    },[response,data])
-
-    return(
-        <h1>Homepage</h1>
-    )
-}
 
 export default Homepage;
