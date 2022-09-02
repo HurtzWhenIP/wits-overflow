bae4ccc859f031c23e224de904bb7543c131afbc
dkb18 <dkbhoora01@gmail.com>
2022-09-02 20:27:57 +0200

    UI increment

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,6 @@
 import useAxios from "../hooks/useAxios";
 import axios from '../apis/ForumServer';
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import '../styles/Homepage.css'
 import { FcSearch } from "react-icons/fc";
 import Loading from "../components/Loading";
@@ -8,58 +8,64 @@ import Loading from "../components/Loading";
 
 function Homepage() {
     //state to hold question data
-    const [data,setData] = useState([]);
+    const [data, setData] = useState([]);
     const [isOpen, setIsopen] = useState(false);
     const [query, setQuery] = useState("All");
 
-    const [response,error,loading,refetch] = useAxios({
+    const [response, error, loading, refetch] = useAxios({
         axiosInstance: axios,
         method: 'GET',
         url: 'getAllQuestions.php',
     });
 
     useEffect(() => {
-        const result = Array.isArray(response) && Array(response);
-        if(response && (result.length>0)){
+        const result = Array.isArray(response);
+        if (response && (result.length > 0)) {
             console.log("Questions stored");
             setData(result);
         }
 
-        return(() => {console.log(data);})
-    },[response])
+        return (() => { console.log(data) })
+    }, [response])
 
     return (
+        <>
+            <div className="wrapper">
+                {loading && <Loading caption={"Loading Data..."} />}
+                <h2>Search For a Question</h2>
+                <div className="search_box">
+                    <div className="dropdown_items" onClick={() => {
+                        setIsopen(!isOpen);
+                    }}>
+                        <div className="default_option">{query}</div>
+                        <ul className={isOpen ? "ul_active" : "ul_search"} >
 
-        <div className="wrapper">
-            {loading && <Loading caption={"Loading Data..."}/>}
-            <h2>Search For a Question</h2>
-            <div className="search_box">
-                <div className="dropdown_items" onClick={() => {
-                    setIsopen(!isOpen);
-                }}>
-                    <div className="default_option">{query}</div>
-                    <ul className={isOpen ? "ul_active" : "ul_search"} >
+                            < div className='li_search' key={0} onClick={() => {
+                                setQuery("All");
+                            }} >All </div>
+                            <div className="li_search" key={1} onClick={() => {
+                                setQuery("Recent");
+                            }} >Recent </div>
+                            <div className="li_search" key={2} onClick={() => {
+                                setQuery("Popular");
+                            }}>Popular</div>
 
-                        < div className = 'li_search' key={0} onClick={() => {
-                            setQuery("All");
-                        }} >All </div>
-                        <div className="li_search" key={1} onClick={() => {
-                            setQuery("Recent");
-                        }} >Recent </div>
-                        <div className="li_search" key={2} onClick={() => {
-                            setQuery("Popular");
-                        }}>Popular</div>
-
-                    </ul>
+                        </ul>
+                    </div>
+                    <div className="search_field">
+                        <input type="text" className="input" placeholder="Search" />
+                        <i className="fas fa-search"></i>
+                    </div>
+                    <button className="search_button">{<FcSearch size={35} />}</button>
                 </div>
-                <div className="search_field">
-                    <input type="text" className="input" placeholder="Search" />
-                    <i className="fas fa-search"></i>
-                </div>
-                <button className="search_button">{<FcSearch size={35}/>}</button>
             </div>
-            
-        </div>
-    )};
+
+
+            <ul class="tilesWrap">
+               
+            </ul>
+        </>
+    );
+};
 
 export default Homepage;
