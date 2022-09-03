84bf31c91720b04b07c02fb56d223a54bd88e366
dkb18 <dkbhoora01@gmail.com>
2022-09-03 12:18:35 +0200

    Display questions data

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,39 +1,40 @@
-import { useEffect,useState } from "react";
-
-function Questions({data}){
+import { useEffect, useState } from "react";
+import '../styles/Questions.css';
+function Questions({ data }) {
     //stores question set 
-    const [questions,setQuestions] = useState(null);
+    const [questions, setQuestions] = useState(null);
 
     //updates questions set when necesarry
     useEffect(() => {
         try {
-            if(data[0].length > 0){
+            if (data[0].length > 0) {
                 console.log("Got Data");
                 setQuestions(data[0]);
             }
         } catch (error) {
             console.log("Data in Transition");
         }
-    },[data])
-    
-    return(
+    }, [data])
+
+    return (
         <div>
-        
-        {data[0].map((question) => {
-          const list = (
-            <>
-              <ul>
-                <li>Id: {question.PostID}</li>
-                <li>Name: {question.UserID}</li>
-                <li>Age: {question.PostTitle}</li>
-                <li>City: {question.PostContent}</li>
-              </ul>
-              <hr />
-            </>
-          );
-          return list;
-        })}
-      </div>
+
+            {questions && questions.map((question) => {
+                const list = (
+                    <>
+                        <ul className="tilesWrap">
+                            <h2>{question.PostID}</h2>
+                            <h3>{question.PostTitle}</h3>
+                            <p>{question.PostContent}</p>
+                            <h3>{question.Solved}</h3>
+                            <button>Read more</button>
+                        </ul>
+                        <hr />
+                    </>
+                );
+                return list;
+            })}
+        </div>
     )
 }
 
