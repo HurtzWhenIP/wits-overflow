e0906f3f915212e37e61605b7e604a9347ce0b93
root-mrrobot <2359325@students.wits.ac.za>
2022-09-06 21:09:46 +0200

    Add Comments

--- php-scripts/deleteUser.php
+++ php-scripts/deleteUser.php
@@ -4,7 +4,7 @@ require_once "db.php";
 $json = $json[$payloadLabel];
 $userID = $json['UserID'];
 
-// Generating and executing the SQL
+// Deleting user from the database
 $sql = "DELETE FROM User WHERE (UserID = ?)";
 $query = $db->prepare($sql);
 $query->bind_param("i", $userID);
